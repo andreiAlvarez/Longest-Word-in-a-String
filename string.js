5f3250b1cc2e3903db490efc03ab5a59f061bf86
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-11 16:04:30 -0400

    Create string.js

---
+++ string.js
@@ -0,0 +1,16 @@
+//solution 1
+function findLongestWordLength(str) {
+  var words = str.split(' ');
+  var maxLength = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+//clever
+const findLongestWordLength = str => Math.max(...str.split(" ").map(word => word.length));
